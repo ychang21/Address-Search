fcfc86b1962c44c4bb333f84babee172ecc8d32c
ychang21 <isabellhchang@gmail.com>
2016-12-15 20:05:59 -0500

    first commit

---
+++ app/Components/Children/Saved.js
@@ -0,0 +1,30 @@
+// Include React 
+var React = require('react');
+
+// Component creation
+var Saved = React.createClass({
+
+	// Here we render the function
+	render: function(){
+
+		return(
+
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h3 className="panel-title text-center">Saved</h3>
+					</div>
+					<div className="panel-body text-center">
+
+							{/*h1>Saved</h1>*/}
+							<p>{this.props.address}</p>
+
+					</div>
+				</div>
+
+
+		)
+	}
+});
+
+// Export the component back for use in other files
+module.exports = Saved;
